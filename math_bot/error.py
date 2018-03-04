6022bf2952ec31b10dddc6b878e44f71efc2b5e1
Dmitry <vip.shoroch@gmail.com>
2018-03-04 15:45:11 +0300

    Retring after network error was disabled

--- math_bot/error.py
+++ math_bot/error.py
@@ -1,4 +1,3 @@
-from telegram.ext import Dispatcher
 from telegram.error import TelegramError, NetworkError
 from time import sleep
 import logging
@@ -11,10 +10,8 @@ def handle_errors(bot, update, error):
         logging.exception('Update %s caused %s error' % (update, error))
         sleep(1)
         bot.send_message(
-            text='A network error occurred. Trying to answer again',
+            text='A network error occurred',
             chat_id=update.message.chat_id
         )
-        dispatcher = Dispatcher.get_instance()
-        dispatcher.process_update(update)
     except TelegramError:
         logging.exception('Update %s caused %s error' % (update, error))
