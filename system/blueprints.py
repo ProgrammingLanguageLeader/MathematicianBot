7c364c636698b1fd12a79543b2131f6fa2e44118
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 04:06:15 +0300

    blueprint modules renaming

--- system/blueprints.py
+++ system/blueprints.py
@@ -1,7 +1,7 @@
 def register_blueprints(app):
     with app.app_context():
-        from web_site.routes import web_site_routes
-        from telegram_bot.routes import telegram_blueprint
+        from web_site.blueprint import web_site_blueprint
+        from telegram_bot.blueprint import telegram_blueprint
 
-        app.register_blueprint(web_site_routes)
+        app.register_blueprint(web_site_blueprint)
         app.register_blueprint(telegram_blueprint, url_prefix='/telegram')
