f559b7b6b53bce9da8152bc1550896dee01bc8e8
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-04-28 00:31:31 +0300

    NotFound component

---
+++ web_site/frontend/src/components/pages/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+<b-container class="pt-4">
+  <h1>Page not found</h1>
+</b-container>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style scoped>
+</style>
