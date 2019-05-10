8484eee45935f8cfeb60ba5319718b3ebaf9ce1b
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-05-10 18:46:43 +0300

    AnswerImage component

---
+++ frontend/src/components/common/AnswerImage.vue
@@ -0,0 +1,24 @@
+<template>
+<b-container class="mt-2 mb-2" v-if="answer">
+  <b-row align-h="center">
+    <h4>Answer</h4>
+  </b-row>
+  <b-row align-h="center">
+    <b-img id="answer-img" :src="answer" />
+  </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+  props: ['answer'],
+  name: 'StyledInputGroup'
+}
+</script>
+
+<style scoped>
+#answer-img {
+  max-width: 100%;
+  width: auto;
+}
+</style>
