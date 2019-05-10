4bad5584297554e6d1bdebe070303fd4904c02a0
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-05-10 04:47:33 +0300

    frontend folder is on the top level now

--- system/blueprints.py
+++ system/blueprints.py
@@ -1,9 +1,7 @@
 def register_blueprints(app):
     with app.app_context():
-        from web_site.blueprint import web_site_blueprint
         from telegram_bot.blueprint import telegram_blueprint
         from api.blueprint import api_blueprint
 
-        app.register_blueprint(web_site_blueprint)
         app.register_blueprint(telegram_blueprint, url_prefix='/telegram')
         app.register_blueprint(api_blueprint, url_prefix='/api')
