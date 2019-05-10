75998ad59b3d98c870adaaf40a52d4a3d79f3d91
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-05-10 03:14:18 +0300

    simple api blueprint implementation

--- api/blueprint.py
+++ api/blueprint.py
@@ -1,5 +1,10 @@
-from flask import Blueprint
+from io import BytesIO
+
+from flask import Blueprint, jsonify, request, current_app, send_file
 from flask_cors import CORS
+from werkzeug.exceptions import BadRequest
+
+from wolfram.api import make_simple_wolfram_request
 
 api_blueprint = Blueprint('api_blueprint', __name__)
 CORS(api_blueprint)
@@ -7,14 +12,15 @@ CORS(api_blueprint)
 
 @api_blueprint.route('/')
 def check():
-    return 'API root'
+    return jsonify('API root')
 
 
 @api_blueprint.route('/simple')
 def make_simple_request():
-    return 'simple'
-
-
-@api_blueprint.route('/detailed')
-def make_detailed_request():
-    return 'detailed'
+    app_id = current_app.config.get('WOLFRAM_APP_ID')
+    wolfram_request = request.args.get('request')
+    if not wolfram_request:
+        raise BadRequest('request must be set')
+    response = make_simple_wolfram_request(wolfram_request, app_id, width=300)
+    response_bytes_io = BytesIO(response)
+    return send_file(response_bytes_io, mimetype='image/gif')
